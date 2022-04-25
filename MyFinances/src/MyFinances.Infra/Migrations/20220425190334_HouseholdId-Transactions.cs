5d3a2b4baccdcb278540bd1e2ddbfc939c79f9f7
gusavilasilva@gmail.com <gusavilasilva@gmail.com>
2022-04-25 16:05:10 -0300

    Fixing transaction entity migration

---
+++ MyFinances/src/MyFinances.Infra/Migrations/20220425190334_HouseholdId-Transactions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinances.Infra.Migrations
+{
+    public partial class HouseholdIdTransactions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HouseholdId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HouseholdId",
+                table: "Transactions");
+        }
+    }
+}
