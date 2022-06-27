14a9ab729e43c3f48c6738e71cd1911d11363273
Gustavo Silva <gusavilasilva@gmail.com>
2022-06-26 21:48:01 -0300

    adding url from configuration to authorization message handler

--- MyFinances/src/MyFinances.Blazor/Client/CustomAuthorizationMessageHandler.cs
+++ MyFinances/src/MyFinances.Blazor/Client/CustomAuthorizationMessageHandler.cs
@@ -6,11 +6,14 @@ namespace MyFinances.Blazor.Client
     public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
     {
         public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
-            NavigationManager navigationManager)
+            NavigationManager navigationManager, 
+            IConfiguration configuration)
             : base(provider, navigationManager)
         {
+            var apiUrl = configuration.GetValue<string>("API_URL");
+
             ConfigureHandler(
-                authorizedUrls: new[] { "https://localhost:8080/api" },
+                authorizedUrls: new[] { apiUrl },
                 scopes: new[] { "api://5e064a37-77e5-4456-ad30-d6070c440614/System.FullAccess" }
 );
         }
